c63c1f20886599a6a81ad95319769824a52eac74
mlobf <marcoslemeborbafilho@gmail.com>
2021-04-04 19:48:03 -0300

    Try to implement some decorator at by async way

--- techWithTim/tutorial_async.py
+++ techWithTim/tutorial_async.py
@@ -11,19 +11,18 @@ import asyncio
 # Do not forget the event loop.
 # To  execute a coroutine you have to use await.
 # Its also mandatory create a event loop.
-#
 
 
 async def main():
     print("Marcos")
     task = asyncio.create_task(foo("text"))
-    await task
+    await asyncio.sleep(0.5)
     print("finish")
 
 
 async def foo(text):
     print(text)
-    await asyncio.sleep(1)  # This thing - the await -  here return a coroutine
+    await asyncio.sleep(6)  # This thing - the await -  here return a coroutine
 
 
 asyncio.run(main())  # Star the event loop here
