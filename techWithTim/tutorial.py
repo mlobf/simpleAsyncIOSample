43872de6974ff98444506316d7727c021aea2e5f
mlobf <marcoslemeborbafilho@gmail.com>
2021-04-04 15:19:56 -0300

    asyncio tutorial

--- techWithTim/tutorial.py
+++ techWithTim/tutorial.py
@@ -10,3 +10,14 @@ import asyncio
     -AsyncIO Module.
 '''
 
+# Below ..... some sync programing.
+#   that has the performance related to clock´s processor.
+#   By default , Python use's only one processor.
+#   But this can be changed using others methods.
+
+def foo():
+  return
+
+foo()
+print('Tim')
+# End of sync programming.
---
+++ techWithTim/tutorial_async.py
@@ -0,0 +1,29 @@
+import asyncio
+
+# Create a coroutine.
+"""
+  Is a wrap version of a function the allows 
+    to run async.
+  
+  stop at 15:39
+"""
+# To use the await keyword, this must be done inside of async function.
+# Do not forget the event loop.
+# To  execute a coroutine you have to use await.
+# Its also mandatory create a event loop.
+#
+
+
+async def main():
+    print("Marcos")
+    task = asyncio.create_task(foo("text"))
+    await task
+    print("finish")
+
+
+async def foo(text):
+    print(text)
+    await asyncio.sleep(1)  # This thing - the await -  here return a coroutine
+
+
+asyncio.run(main())  # Star the event loop here
